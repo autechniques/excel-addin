a76b2335c1498a63e2bb81a039f6b5003d7998fa
AU Support <Khanhdd101@gmail.com>
2022-12-14 12:55:14 +0700

    update

---
+++ match.c
@@ -0,0 +1,27 @@
+<!-- 850*360 -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>MATCH.C</title>
+<link rel="stylesheet" type="text/css" href="function/code-inner.css">
+<link rel="stylesheet" type="text/css" href="function/macros.css">
+</head>
+<body>
+<h1 >MATCH.C function
+<button class="button button4" style="float: right;"><b>Microsoft 365</b></button>
+</h1>
+
+<p><div>Trả về vị trí (cột) xuất hiện của một mảng nằm trong mảng chỉ định.</div></p>
+
+<img src="gif/match.c.gif">
+<blockquote>
+    <p>&nbsp;VD1: xem ảnh gif minh họa.</p>
+</blockquote>
+
+<p><strong>Related function</strong></p>
+
+<p>
+<a href="match.r.html">MATCH.R</a>&nbsp;&nbsp;&nbsp;Trả về vị trí (hàng) xuất hiện của một mảng nằm trong mảng chỉ định.
+</p>
+
+<p>Return to <a href="index.html">Home</a></p>
+
+</body></html>
---
+++ match.r
@@ -0,0 +1,28 @@
+<!-- 850*360 -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>MATCH.R</title>
+    <link rel="stylesheet" type="text/css" href="function/code-inner.css">
+    <link rel="stylesheet" type="text/css" href="function/macros.css">
+    </head>
+    <body>
+    <h1 >MATCH.R function
+    <button class="button button4" style="float: right;"><b>Microsoft 365</b></button>
+    </h1>
+    
+    <p><div>Trả về vị trí (hàng) xuất hiện của một mảng nằm trong mảng chỉ định.</div></p>
+    
+    <img src="gif/match.r.gif">
+    <blockquote>
+        <p>&nbsp;VD1: xem ảnh gif minh họa.</p>
+    </blockquote>
+    
+    <p><strong>Related function</strong></p>
+    
+    <p>
+    <a href="match.2d.html">MATCH.2D</a>&nbsp;&nbsp;&nbsp;Tìm vị trí hàng, cột của giá trị tìm kiếm trong mảng chỉ định.
+    </p>
+    
+    <p>Return to <a href="index.html">Home</a></p>
+    
+    </body></html>
+    
