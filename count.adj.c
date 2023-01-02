9bf6aa9c9f5e348ed097e9a5be59506386fdb774
AU Support <Khanhdd101@gmail.com>
2023-01-02 17:06:16 +0700

    update

---
+++ count.adj.c
@@ -0,0 +1,27 @@
+<!-- 850*360 -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>COUNT.ADJ.C</title>
+<link rel="stylesheet" type="text/css" href="function/code-inner.css">
+<link rel="stylesheet" type="text/css" href="function/macros.css">
+</head>
+<body>
+<h1 >COUNT.ADJ.C function
+<button class="button button4" style="float: right;"><b>Microsoft 365</b></button>
+</h1>
+
+<p><div>iệt kê tất cả ký tự và số lần lặp lại của các ký tự trùng lặp liền kề trong chuỗi.</div></p>
+
+<img src="gif/count.adj.c.gif">
+<blockquote>
+    <p>&nbsp;VD1: xem ảnh gif minh họa.</p>
+</blockquote>
+
+<p><strong>Related function</strong></p>
+
+<p>
+<a href="count.c.html">COUNT.C</a>&nbsp;&nbsp;&nbsp;Liệt kê tất cả ký tự và số lần xuất hiện trong chuỗi.
+</p>
+
+<p>Return to <a href="index.html">Home</a></p>
+
+</body></html>
---
+++ count.c
@@ -0,0 +1,28 @@
+<!-- 850*360 -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>COUNT.C</title>
+    <link rel="stylesheet" type="text/css" href="function/code-inner.css">
+    <link rel="stylesheet" type="text/css" href="function/macros.css">
+    </head>
+    <body>
+    <h1 >COUNT.C function
+    <button class="button button4" style="float: right;"><b>Microsoft 365</b></button>
+    </h1>
+    
+    <p><div>Liệt kê tất cả ký tự và số lần xuất hiện trong chuỗi.</div></p>
+    
+    <img src="gif/count.c.gif">
+    <blockquote>
+        <p>&nbsp;VD1: đếm số lần xuất hiện của dấu ;</p>
+    </blockquote>
+    
+    <p><strong>Related function</strong></p>
+    
+    <p>
+    <a href="count.adj.c.html">COUNT.ADJ.C</a>&nbsp;&nbsp;&nbsp;iệt kê tất cả ký tự và số lần lặp lại của các ký tự trùng lặp liền kề trong chuỗi.
+    </p>
+    
+    <p>Return to <a href="index.html">Home</a></p>
+    
+    </body></html>
+    
