5b5622611496eb8a0597ae40c1044053b34f0652
AU Support <Khanhdd101@gmail.com>
2023-01-06 16:40:20 +0700

    update

---
+++ unique.adj.c
@@ -0,0 +1,27 @@
+<!-- 850*360 -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>UNIQUE.ADJ.C</title>
+<link rel="stylesheet" type="text/css" href="function/code-inner.css">
+<link rel="stylesheet" type="text/css" href="function/macros.css">
+</head>
+<body>
+<h1 >UNIQUE.ADJ.C function
+ <a href="interface.html"><button class="button button4" style="float: right;"><b>Mua ngay</b></button></a>
+</h1>
+
+<p><div>Lấy ra dãy các ký tự (liền kề) trùng lặp từ chuỗi chỉ định.</div></p>
+
+<img src="gif/unique.adj.c.gif">
+<blockquote>
+    <p>&nbsp;VD1: xem ảnh gif minh họa.</p>
+</blockquote>
+
+<p><strong>Related function</strong></p>
+
+<p>
+<a href="unique.c.html">UNIQUE.C</a>&nbsp;&nbsp;&nbsp;Lấy ra dãy ký tự không trùng lặp từ chuỗi chỉ định.
+</p>
+
+<p>Return to <a href="index.html">Home</a></p>
+
+</body></html>
---
+++ unique.c
@@ -0,0 +1,28 @@
+<!-- 850*360 -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>UNIQUE.C</title>
+    <link rel="stylesheet" type="text/css" href="function/code-inner.css">
+    <link rel="stylesheet" type="text/css" href="function/macros.css">
+    </head>
+    <body>
+    <h1 >UNIQUE.C function
+     <a href="interface.html"><button class="button button4" style="float: right;"><b>Mua ngay</b></button></a>
+    </h1>
+    
+    <p><div>Lấy ra dãy ký tự không trùng lặp từ chuỗi chỉ định.</div></p>
+    
+    <img src="gif/unique.c.gif">
+    <blockquote>
+        <p>&nbsp;VD1: xem ảnh gif minh họa.</p>
+    </blockquote>
+    
+    <p><strong>Related function</strong></p>
+    
+    <p>
+    <a href="unique.adj.c.html">UNIQUE.ADJ.C</a>&nbsp;&nbsp;&nbsp;Lấy ra dãy các ký tự (liền kề) trùng lặp từ chuỗi chỉ định.
+    </p>
+    
+    <p>Return to <a href="index.html">Home</a></p>
+    
+    </body></html>
+    
